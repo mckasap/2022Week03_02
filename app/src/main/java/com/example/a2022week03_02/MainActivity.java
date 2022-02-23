97ee746694efd1ea97e13d345dc52f7bac1d652e
mckasap <mckasap@gmail.com>
2022-02-23 11:07:15 +0300

    İkinci Yukleme

--- app/src/main/java/com/example/a2022week03_02/MainActivity.java
+++ app/src/main/java/com/example/a2022week03_02/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.a2022week03_02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setListAdapter( new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.countries)));
+    }
+
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        Toast.makeText(MainActivity.this,(String) l.getItemAtPosition(position),Toast.LENGTH_LONG).show();
     }
 }
